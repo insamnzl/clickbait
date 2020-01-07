101c5ffc57b854ba49e8d465b6614067179bb691
Insa Menzel <menzel@comm.rwth-aachen.de>
2020-01-07 12:03:19 +0100

    Analysescript erstellt

---
+++ AnalyseScript.R
@@ -0,0 +1,17 @@
+#Bibliotheken laden -----
+#install.packages("tidyverse")
+#install.packages("lubridate")
+#install.packages("psych")
+#install.packages("esquisse")
+#install.packages("ggthemes")
+#install.packages("ggplot2")
+#install.packages("jmv")
+#install.packages("likert")
+
+#install.packages("devtools")
+library(devtools)
+devtools::install_github("HCIC/r-tools")
+library(tidyverse)
+library(jmv)
+library(likert)
+
